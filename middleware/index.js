1ae1e647268169d57b32888bcffb026d20aaa765
Anton <asa@dbbsoftware.com>
2021-08-06 16:14:29 +0300

    another commit

--- middleware/index.js
+++ middleware/index.js
@@ -1,33 +1,37 @@
 const sharp = require("sharp");
+const fs = require("fs");
 const uuid = require("uuid");
 
 const fileUploader = (req, res, next) => {
-    let resizedFiles = [];
-    const files = req.files;
-    for (const file of req.files) {
-      const fileId = uuid.v4() + Date.now();
-      resizedFiles.push(
-        new Promise((resolve, reject) => {
-            console.log(file.path);
-          sharp(file.path)
-            .resize(300)
-            .blur()
-            .toFile(`resized/${fileId}.png`, function (err, data) {
-              if (err) return reject(err);
-              resolve(data);
-            });
-        })
-      );
-    }
-
-    Promise.all(resizedFiles)
-      .then((data) => {
-        console.log("File resizing process is complete ", data);
-        req.newFiles = data;
+  let resizedFiles = [];
+  const files = req.files;
+  const readyFiles = [];
+  for (const file of req.files) {
+    const fileId = uuid.v4() + Date.now();
+    const output = "resized/" + fileId + ".png";
+    resizedFiles.push(
+      new Promise((resolve, reject) => {
+        console.log(file.path);
+        sharp(file.path)
+          .resize(300)
+          .blur()
+          .toFile(output, (err, data) => {
+            if (err) return console.log(err);
+            resolve(output);
+          });
       })
-      .catch((err) =>
-        console.log("Error occurs during file resize processing :", err)
-      );
+    );
+  }
+
+  Promise.all(resizedFiles)
+    .then((data) => {
+      console.log(data);
+      req.newFiles = data;
+    })
+    .catch((err) =>
+      console.log("Error occurs during file resize processing :", err)
+    );
+
   next();
 };
 
--- modules/images/ImageService.js
+++ modules/images/ImageService.js
@@ -3,6 +3,7 @@ const sharp =require('sharp')
 const fs = require("fs");
 const path = require("path");
 const uuid = require("uuid");
+const { resolve } = require('path');
 
 module.exports = class User {
   constructor({ postgresDBConnection, awsS3Service }) {
@@ -12,11 +13,25 @@ module.exports = class User {
   }
 
   async create(req) {
-    console.log("the new array to handle:", req.newFiles);
+    // console.log("the new array to handle:", req.newFiles);
     // const folder = '/home/aymarngams/Documents/'
     // const f = fs.readdirSync(folder)[1];
     // const pathFile = folder + f
-    // const fileUri = await this.aws_s3.uploadImage(pathFile, f)
+    const filesToSaves = []
+    if(Array.isArray(req.newFiles) && req.newFiles.length > 0){
+      for (const file of req.newFiles) {
+        filesToSaves,push(new Promise((resolve, reject) =>{
+          const uploadedFile = this.aws_s3.uploadImage(file, path.resolve(file))
+          resolve(uploadedFile)
+        }))
+      }
+
+      Promise.all(filesToSaves).then(data=>{
+        console.log('file to store in the db : ', data);
+      })
+    }else{
+      console.log('not an array ', req.newFiles);
+    }
     return [];
   }
 };
